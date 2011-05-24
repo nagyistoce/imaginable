6cf522c17b6ae9dd5405734b0897a578c87ef55c
Kuzma.Shapran <Kuzma.Shapran@e9aa2dee-2a26-11df-8215-13362a2a91fc>
2011-05-24 09:26:14 +0000

    + tonemap GUI (initial)

---
+++ tonemap_gui/version.hpp
@@ -0,0 +1,56 @@
+/*************
+**
+** Project:      Imaginable :: tonemap (GUI)
+** File info:    $Id$
+** Author:       Copyright (C) 2009 - 2011 Kuzma Shapran <Kuzma.Shapran@gmail.com>
+** License:      GPLv3
+**
+**  This file is part of Imaginable.
+**
+**  Imaginable is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Imaginable is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Imaginable.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************/
+
+
+#ifndef IMAGINABLE__TINEMAP_GUI__VERSION__INCLUDED
+#define IMAGINABLE__TINEMAP_GUI__VERSION__INCLUDED
+
+
+#include <ctime>
+
+
+namespace version {
+
+	unsigned    major      (void);
+	unsigned    minor      (void);
+	const char* revision   (void);
+	unsigned    number     (void);
+	const char* label      (void);
+
+	const char* full_string(void);
+
+	time_t      time       (void);
+
+	unsigned    year       (void);
+	unsigned    month      (void);
+	unsigned    day        (void);
+	unsigned    hour       (void);
+	unsigned    minute     (void);
+	unsigned    second     (void);
+
+	const char* ubuntu_style_string(void);
+	double      ubuntu_style(void);
+}
+
+#endif // IMAGINABLE__TINEMAP_CLI__VERSION__INCLUDED
